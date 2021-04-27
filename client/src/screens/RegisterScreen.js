68971743ed52d74870844a6c9dac68d648d3d2ad
Jay Jhangiani <jayjhangiani@Jays-MacBook-Air.local>
2021-04-27 11:55:29 +0530

    Server

--- client/src/screens/RegisterScreen.js
+++
@@ -1,9 +0,0 @@
-import { useParams } from "react-router-dom";
-import UserRegister from "../components/register/UserRegister";
-
-const RegisterScreen = () => {
-  const { type } = useParams();
-  return <div>{type === "user" ? <UserRegister /> : <h1>{type}</h1>}</div>;
-};
-
-export default RegisterScreen;
