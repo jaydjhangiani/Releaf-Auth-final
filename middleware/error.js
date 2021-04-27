b60388ca5866341ea113a4eca1b0eb68f6be1c9a
Bishwajit Jha <ajitjha393@gmail.com>
2021-04-27 20:12:44 +0530

    Added Image and Pdf Upload feat

--- middleware/error.js
+++ middleware/error.js
@@ -1,24 +1,25 @@
 const ErrorResponse = require('../utils/errorResponse')
 
 const errorHandler = (err, req, res, next) => {
-    let error = { ...err };
-    
-    error.message = err.message;
-  
-    if (err.code === 11000) {
-      const message = `A user already exists`;
-      error = new ErrorResponse(message, 400);
-    }
-  
-    if (err.name === "ValidationError") {
-      const message = Object.values(err.errors).map((val) => val.message);
-      error = new ErrorResponse(message, 400);
-    }
-  
-    res.status(error.statusCode || 500).json({
-      success: false,
-      error: error.message || "Server Error",
-    });
-  };
+  let error = { ...err }
 
-module.exports = errorHandler;
+  console.log(err)
+  error.message = err.message
+
+  if (err.code === 11000) {
+    const message = `A user already exists`
+    error = new ErrorResponse(message, 400)
+  }
+
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map((val) => val.message)
+    error = new ErrorResponse(message, 400)
+  }
+
+  res.status(error.statusCode || 500).json({
+    success: false,
+    error: error.message || 'Server Error',
+  })
+}
+
+module.exports = errorHandler
