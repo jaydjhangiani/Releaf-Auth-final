9327faf514fb3498b34f5fba034272448b50a8d3
Bishwajit Jha <ajitjha393@gmail.com>
2021-04-28 09:54:05 +0530

    Added Endpoint for Fetching All experts info

--- routes/experts.route.js
+++ routes/experts.route.js
@@ -1,7 +1,11 @@
-const express = require("express");
-const router = express.Router();
-const { createExpert } = require("../controllers/experts.controller");
+const express = require('express')
+const router = express.Router()
+const {
+	createExpert,
+	fetchAllExperts,
+} = require('../controllers/experts.controller')
 
-router.route("/register").post(createExpert);
+router.route('/register').post(createExpert)
+router.route('/').get(fetchAllExperts)
 
-module.exports = router;
+module.exports = router
--- server.js
+++ server.js
@@ -1,70 +1,61 @@
-require("dotenv").config({ path: "./config.env" });
-const express = require("express");
-const connectDB = require("./config/db");
-const errorHandler = require("./middleware/error");
-const cors = require("cors");
-const path = require("path");
-const multer = require("multer");
+require('dotenv').config({ path: './config.env' })
+const express = require('express')
+const connectDB = require('./config/db')
+const errorHandler = require('./middleware/error')
+const cors = require('cors')
+const path = require('path')
+const multer = require('multer')
 
 // Connect DB
-connectDB();
+connectDB()
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
 app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+	cors({
+		origin: 'http://localhost:3000',
+	})
+)
 
-app.use("/api/auth/", require("./routes/auth.route"));
-app.use("/api/private/", require("./routes/private.route"));
-app.use("/api/contact/", require("./routes/contact.route"));
+app.use('/api/auth/', require('./routes/auth.route'))
+app.use('/api/private/', require('./routes/private.route'))
+app.use('/api/contact/', require('./routes/contact.route'))
 
-app.use("/", express.static(path.join(__dirname, "/")));
+app.use('/', express.static(path.join(__dirname, '/')))
 
 let storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname)); //Appending extension
-  },
-});
+	destination: function (req, file, cb) {
+		cb(null, 'uploads/')
+	},
+	filename: function (req, file, cb) {
+		cb(null, Date.now() + path.extname(file.originalname)) //Appending extension
+	},
+})
 
-var upload = multer({ storage: storage });
+var upload = multer({ storage: storage })
 
 var dataUpload = upload.fields([
-  { name: "profilePicture", maxCount: 1 },
-  { name: "resume", maxCount: 1 },
-]);
+	{ name: 'profilePicture', maxCount: 1 },
+	{ name: 'resume', maxCount: 1 },
+])
 
-console.log("Serverjs");
-app.use(
-  "/api/expert",
-  dataUpload,
-  (req, res, next) => {
-    console.log("after");
-    next();
-  },
-  require("./routes/experts.route")
-);
+app.use('/api/expert', dataUpload, require('./routes/experts.route'))
 
 /* File Upload Routes */
 
 // Error Handler last piece of middleware
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 
 const server = app.listen(PORT, () =>
-  console.log(`server running on port ${PORT}`)
-);
+	console.log(`server running on port ${PORT}`)
+)
 
-process.on("unhandledRejection", (err, promise) => {
-  console.log(`Logged Error: ${err}`);
-  server.close(() => process.exit(1));
-});
+process.on('unhandledRejection', (err, promise) => {
+	console.log(`Logged Error: ${err}`)
+	server.close(() => process.exit(1))
+})
